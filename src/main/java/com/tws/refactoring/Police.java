815b1104fcbf81ff84e6a5d5402aa60a2d44d642
Wong Cho Hing <venus.wong@oocl.com>
2018-11-26 15:25:41 +0800

    Refactor: set default return boolean driverAgeValid = false in method checkDriverAge in class Police, and modify the return statement in the if condition

--- src/main/java/com/tws/refactoring/Police.java
+++ src/main/java/com/tws/refactoring/Police.java
@@ -3,8 +3,11 @@ package com.tws.refactoring;
 public class Police {
     public boolean checkDriverAge(Driver driver) {
         final int validDriverAge = 18;
+        boolean driverAgeValid = false;
 
-        if(driver.getDriverAge() >= validDriverAge) return true;
-        else return false;
+        if(driver.getDriverAge() >= validDriverAge){
+            driverAgeValid = true;
+        }
+        return driverAgeValid;
     }
 }
